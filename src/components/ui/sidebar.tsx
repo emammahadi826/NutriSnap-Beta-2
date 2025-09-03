56305cfd9f90f87ea64db769ed54a86803d9d55c
emam mahadi <emammahadi826@gmail.com>
2025-09-03 13:22:02 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <UserInfo>
    
    with t

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -276,7 +276,6 @@ const SidebarTrigger = React.forwardRef<
       variant="ghost"
       size="icon"
       className={cn("h-7 w-7 data-[state=collapsed]:flex",
-      state === 'expanded' ? '' : 'mx-auto',
       className
       )}
       onClick={(event) => {
@@ -519,7 +518,7 @@ const SidebarMenuItem = React.forwardRef<
 SidebarMenuItem.displayName = "SidebarMenuItem"
 
 const sidebarMenuButtonVariants = cva(
-  "peer/menu-button flex items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:justify-center [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
+  "peer/menu-button flex items-center gap-2 overflow-hidden rounded-md p-2 outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!flex group-data-[collapsible=icon]:!size-10 group-data-[collapsible=icon]:!items-center group-data-[collapsible=icon]:!justify-center [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -530,7 +529,7 @@ const sidebarMenuButtonVariants = cva(
       size: {
         default: "h-8 text-sm",
         sm: "h-7 text-xs",
-        lg: "h-12 group-data-[state=expanded]:w-full text-sm group-data-[collapsible=icon]:!size-10 group-data-[collapsible=icon]:!items-center group-data-[collapsible=icon]:!justify-center",
+        lg: "h-12 text-sm group-data-[state=expanded]:w-full",
       },
     },
     defaultVariants: {
@@ -773,5 +772,6 @@ export {
     
 
     
+
 
 
