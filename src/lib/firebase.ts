d2d97723c85c995e3da9a863b0db277299f062c4
emam mahadi <emammahadi826@gmail.com>
2025-09-07 09:53:19 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -6,16 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCkb30KEyc90T5TWAHmqyudbLZo6rhU5hY",
-  authDomain: "nutriscan-95ab1.firebaseapp.com",
-  databaseURL: "https://nutriscan-95ab1-default-rtdb.firebaseio.com",
-  projectId: "nutriscan-95ab1",
-  storageBucket: "nutriscan-95ab1.firebasestorage.app",
-  messagingSenderId: "171444288043",
-  appId: "1:171444288043:web:55a4fc9e02b16b0c2f886e",
-  measurementId: "G-C62JB65K1S"
+  apiKey: "AIzaSyCvN0lANt903JVyT_3HCs_NI2nV4I6af6I",
+  authDomain: "nutrisnap-vdek6.firebaseapp.com",
+  projectId: "nutrisnap-vdek6",
+  storageBucket: "nutrisnap-vdek6.firebasestorage.app",
+  messagingSenderId: "1033925483096",
+  appId: "1:1033925483096:web:d2a77afc87361216bfc82d"
 };
 
 // Initialize Firebase
